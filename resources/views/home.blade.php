06b2a63522f06b5bc3cbc80974dd9012adf3d9a2
Mindaugas Azubalis <mindaugas.azubalis@gmail.com>
2016-10-18 15:06:36 +0000

    start of the middleware added butstilll not working

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,13 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
+            @if(\Auth::user()->admin == 1 )
+            <p>Logged in as <strong style="color: red;">Admin</strong>: <strong>{{ Auth::user()->name }}</strong></p>
+            @else
+            <p>You are logged in as <strong>{{ Auth::user()->name }}</strong>!</p>
+            @endif
+        </div>
+        <div class="col-md-10 col-md-offset-1">
             @include('example.table_books')           
             @include('example.table_authors')
             @include('example.table_categories')
